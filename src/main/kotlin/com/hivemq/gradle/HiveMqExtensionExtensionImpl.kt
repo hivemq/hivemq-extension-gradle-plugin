980da1666f98b1549fbef724d3ea54b70b031f3b
Silvio Giebl <silvio.giebl@hivemq.com>
2020-09-11 15:40:27 +0200

    Added generation of service descriptor

--- src/main/kotlin/com/hivemq/gradle/HiveMqExtensionExtensionImpl.kt
+++ src/main/kotlin/com/hivemq/gradle/HiveMqExtensionExtensionImpl.kt
@@ -8,8 +8,7 @@ open class HiveMqExtensionExtensionImpl : HiveMqExtensionExtension {
     override var author: String? = null
     override var priority: Int? = null
     override var startPriority: Int? = null
-
+    override var mainClass: String? = null
     override var sdkVersion: String? = null
-
     override var customJarTask: String? = null
 }
