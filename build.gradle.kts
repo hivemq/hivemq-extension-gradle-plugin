7ab4525ca250362c4c0cdd9030de4a09451fbd13
Silvio Giebl <silvio.giebl@hivemq.com>
2020-09-12 20:24:55 +0200

    Small refactorings

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,7 +66,7 @@ gradlePlugin {
             id = "${group}.${name}"
             displayName = metadata.readableName
             description = project.description
-            implementationClass = "${group}.gradle.HiveMqExtensionPlugin"
+            implementationClass = "${group}.gradle.HiveMQExtensionPlugin"
         }
     }
 }
