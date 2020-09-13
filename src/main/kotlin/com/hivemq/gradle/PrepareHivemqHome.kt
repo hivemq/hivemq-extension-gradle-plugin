0c51919b89cb9d719a0562bc567e169b9a58e717
Silvio Giebl <silvio.giebl@hivemq.com>
2020-09-13 21:52:34 +0200

    Added prepareHivemqHome and runHivemqWithExtension tasks

---
+++ src/main/kotlin/com/hivemq/gradle/PrepareHivemqHome.kt
@@ -0,0 +1,14 @@
+package com.hivemq.gradle
+
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Sync
+import org.gradle.api.tasks.bundling.Zip
+
+/**
+ * @author Silvio Giebl
+ */
+open class PrepareHivemqHome : Sync() {
+
+    val hivemqFolder: Property<Any> = project.objects.property(Any::class.java)
+    val extensionZipTask: Property<Zip> = project.objects.property(Zip::class.java)
+}
