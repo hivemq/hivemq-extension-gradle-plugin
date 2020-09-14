407601f71c4999b02b95c6c74222c51812ecee5c
Silvio Giebl <silvio.giebl@hivemq.com>
2020-09-14 11:07:19 +0200

    fixed default priority: 1000 -> 0

--- src/main/kotlin/com/hivemq/gradle/HivemqExtensionExtensionImpl.kt
+++ src/main/kotlin/com/hivemq/gradle/HivemqExtensionExtensionImpl.kt
@@ -7,7 +7,7 @@ package com.hivemq.gradle
 open class HivemqExtensionExtensionImpl : HivemqExtensionExtension {
     override var name: String? = null
     override var author: String? = null
-    override var priority: Int = 1000
+    override var priority: Int = 0
     override var startPriority: Int = 1000
     override var mainClass: String? = null
     override var sdkVersion: String = "latest.integration"
