22043e04228099d85486138bf3cb8a20b703733c
Silvio Giebl <silvio.giebl@hivemq.com>
2022-09-11 22:58:18 +0200

    Update build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
     plugins {
         id("com.gradle.plugin-publish") version "${extra["plugin.plugin-publish.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
+        id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
         id("com.github.sgtsilvio.gradle.metadata") version "${extra["plugin.metadata.version"]}"
     }
 }
